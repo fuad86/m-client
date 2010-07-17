bacc95eba70a20f00c77461862b1d6dd0b5ae42f
MichalKaluzny@gmail.com <MichalKaluzny@gmail.com@4783ef28-d2be-50eb-4228-48550c1bc54b>
2010-07-17 23:17:35 +0000

    1st revision. Forked from private repo

---
+++ trunk/mClient/Config.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using mClient.Constants;
+
+namespace mClient
+{
+    public static class Config
+    {
+        public static string Login;
+        public static string Password;
+        public static string Host;
+        public static WoWVersion Version;
+        public static long LogFilter;
+        public static bool Retail;
+        public static bool LogToFile;
+
+        static Config()
+        {
+            Login = "JUSTMAKU";
+            Password = "ORZECH";
+            //Host = "logon.unitedwow.com";
+            //Host = "eu.logon.worldofwarcraft.com";
+            Host = "realm.neverendless-wow.com";
+            //Host = "localhost";
+
+            Version.major = 3;
+            Version.minor = 1;
+            Version.update = 3;
+            Version.build = 9947;
+
+
+            Retail = false;
+
+            LogFilter = 0x0000000011000000;
+            LogToFile = true;
+        }
+    }
+}
---
+++ trunk/mClient/Utils/Log.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace mClient.Shared
+{
+    public static class Log
+    {
+        static StreamWriter packetFile = File.AppendText("log_packets.txt");
+        static StreamWriter logFile = File.AppendText("log_all.txt");
+
+        public static void WriteLine(LogType type, string format, params object[] parameters)
+        {
+
+
+            format = string.Format("[{0}][{1}]{2}", Time.GetTime(), type, format);
+            string msg = string.Format(format, parameters);
+
+            if (Config.LogToFile)
+            {
+                if (type == LogType.Packet)
+                {
+                    packetFile.WriteLine(parameters[0].ToString());
+                    packetFile.Flush();
+                }
+                else
+                {
+                    logFile.WriteLine(msg);
+                    packetFile.Flush();
+                }
+            }
+
+            if (((UInt32)type & Config.LogFilter) > 0)
+                return;
+            else
+                mCore.Event(new Event(EventType.EVENT_LOG, "0", new object[] { msg }));
+
+        }
+
+    }
+
+    public enum LogType : long
+    {
+        Command = 0x1000000000000000,
+        Normal  = 0x0100000000000000,
+        Success = 0x0010000000000000,
+        Error   = 0x0001000000000000,
+        Debug   = 0x0000100000000000,
+        Test    = 0x0000010000000000,
+        Chat    = 0x0000001000000000,
+        Terrain = 0x0000000100000000,
+        Network = 0x0000000010000000,
+        Packet  = 0x0000000001000000,
+    }
+
+}
